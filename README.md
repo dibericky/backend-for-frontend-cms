1de2d14ccfb1e9e1ea735d3b7edcb203091c7a0c
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-12-28 21:58:34 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Backend for Frontend for [REACT-CMS](https://github.com/dibericky/backend-for-frontend-cms.git)
---
+++ src/Application.kt
@@ -0,0 +1,102 @@
+package com.cms.bff.dibericky
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.locations.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import io.ktor.http.ContentDisposition.Companion.File
+import kotlinx.coroutines.*
+
+fun String.asResource() = this.javaClass::class.java.getResource(this).readText()
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Locations) {
+    }
+
+    val client = HttpClient(Apache) {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+    }
+    runBlocking {
+        // Sample for making a HTTP Client request
+        /*
+        val message = client.post<JsonSampleClass> {
+            url("http://127.0.0.1:8080/path/to/endpoint")
+            contentType(ContentType.Application.Json)
+            body = JsonSampleClass(hello = "world")
+        }
+        */
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+
+        get<Collections>{
+            val json = "/mocks_data/collections.json".asResource()
+            call.respondText(json)
+        }
+        get<Configs>{
+            val json = "/mocks_data/configs.json".asResource()
+            call.respondText(json)
+        }
+        get<CustomViews>{
+            val json = "/mocks_data/custom-views.json".asResource()
+            call.respondText(json)
+        }
+
+        get<MyLocation> {
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
+    }
+}
+
+@Location("/collections")
+class Collections()
+
+@Location("/configs")
+class Configs()
+
+@Location("/custom-views")
+class CustomViews()
+
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+
+@Location("/type/{name}") data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+data class JsonSampleClass(val hello: String)
+
